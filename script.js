02dce6778e0d34e2b4a9bbc0e85a55d1e386f79e
renongz <hirayaalipio@gmail.com>
2025-03-21 10:44:43 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,11 @@
-class NavigationEffect {
-  constructor(navigation) {
-    this.previous = null;
-    this.current = null;
-    this.navigation = navigation;
-    this.anchors = this.navigation.querySelectorAll("a");
+const container = document.querySelector('.container');
+const registerBtn = document.querySelector('.register-btn');
+const loginBtn = document.querySelector('.login-btn');
 
-    this.anchors.forEach((anchor) => {
-      anchor.addEventListener("click", (e) => {
-        this.handlePrevious();
-        this.handleCurrent(anchor);
-      });
-    });
-  }
+registerBtn.addEventListener('click', () => {
+    container.classList.add('active');
+})
 
-  handleCurrent(current) {
-    this.current = current;
-    this.current.classList.toggle("active");
-    const nodes = this.getNodes(this.current);
-
-    gsap.to(nodes[0], {
-      duration: 3,
-      ease: "elastic.out(1.4, 0.4)",
-      yPercent: "-100",
-      stagger: 0.012,
-      overwrite: true
-    });
-
-    gsap.to(nodes[1], {
-      duration: 3,
-      ease: "elastic.out(1.4, 0.4)",
-      yPercent: "100",
-      stagger: 0.012,
-      overwrite: true
-    });
-  }
-
-  handlePrevious() {
-    this.previous = document.querySelector(".active");
-    if (this.previous) {
-      this.previous.classList.toggle("active");
-      const nodes = this.getNodes(this.previous);
-      gsap.to(nodes[0], {
-        duration: 0.2,
-        ease: "power1.out",
-        yPercent: "0",
-        overwrite: true,
-        stagger: 0.012
-      });
-
-      gsap.to(nodes[1], {
-        duration: 0.2,
-        ease: "power1.out",
-        yPercent: "0",
-        overwrite: true,
-        stagger: 0.012
-      });
-    }
-  }
-
-  getNodes(item) {
-    return [
-      gsap.utils.selector(item)(".blue rect"),
-      gsap.utils.selector(item)(".pink rect")
-    ];
-  }
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  new NavigationEffect(document.querySelector("nav"));
-});
+loginBtn.addEventListener('click', () => {
+    container.classList.remove('active');
+})
